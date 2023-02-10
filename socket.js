09f432f43c24a44b55c59cf4a8759f314121cfe4
phonewaiyamhein <phonewaiyamhein@ayainnovation.com>
2023-02-10 15:59:56 +0630

    game init

--- socket.js
+++ socket.js
@@ -1,44 +1,51 @@
 import message from "./models/message.js";
 import gameModel from "./models/game.js";
+import initialState from "./models/initialState.js";
 
 export default function configureSocket(io) {
-  io.on("connection", (socket) => {
-    // const session = socket.request.session;
-    // console.log("session", session);
+    io.on("connection", (socket) => {
+        // const session = socket.request.session;
+        // console.log("session", session);
 
-    socket.on("enter-room", ({ roomId, userId }, callback) => {
-      socket.join(roomId);
-      socket.userId = userId;
-      // callback(`user ${userId} joined room ${roomId}`);
-    });
-
-    socket.on("leave-room", ({ roomId, userId }, callback) => {
-      socket.leave(roomId);
-      socket.userId = userId;
-      // callback(`user ${userId} left room ${roomId}`);
-    });
-
-    socket.on(
-      "send-message",
-      async ({ sender, body, game, room, type = "text", inGame = false }, callback) => {
-        const payload = {
-          sender,
-          body,
-          room,
-          type,
-          inGame
-        };
-        game = await gameModel.findById(game).populate("template");
-        if (game) {
-          payload.game = game;
-        }
-        let newMessage = await message.create(payload);
-        newMessage = await newMessage.populate({
-          path: "sender",
+        socket.on("enter-room", ({ roomId, userId }, callback) => {
+            socket.join(roomId);
+            socket.userId = userId;
+            // callback(`user ${userId} joined room ${roomId}`);
         });
-        callback(newMessage)
-        socket.to(room).emit("receive-message", newMessage);
-      }
-    );
-  });
+
+        socket.on("leave-room", ({ roomId, userId }, callback) => {
+            socket.leave(roomId);
+            socket.userId = userId;
+            // callback(`user ${userId} left room ${roomId}`);
+        });
+
+        socket.on(
+            "send-message",
+            async ({ sender, body, game, room, type = "text", inGame = false }, callback) => {
+                const payload = {
+                    sender,
+                    body,
+                    room,
+                    type,
+                    inGame
+                };
+                game = await gameModel.findById(game).populate("template");
+                if (game) {
+                    payload.game = game;
+                }
+                let newMessage = await message.create(payload);
+                newMessage = await newMessage.populate({
+                    path: "sender",
+                });
+                callback(newMessage)
+                socket.to(room).emit("receive-message", newMessage);
+            }
+        );
+
+
+        socket.on("start-game", async ({ game }, callback) => {
+            console.log(game)
+
+        })
+    });
 }
