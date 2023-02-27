aee3d1c2f59a0593b7d17ac1211a0519fd388832
phonewaiyamhein <phonewaiyamhein@ayainnovation.com>
2023-02-27 16:04:57 +0630

    avatar

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ const userSchema = new Schema(
     username: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: false },
-    avatar: { type: String, required: false },
+    avatar: { type: Object, required: false },
     banner: { type: String, required: false },
     bio: { type: String, required: false },
     isActive: { type: Boolean, required: true, default: true },
